c81846ee06b198d2a2c3e6c17ad5bd74aa8f72cf
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2022-03-06 10:30:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -36,7 +36,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double value = 0;
-  for (int i = 1; i <= count; i++) {
+  for (uint16_t i = 1; i <= count; i++) {
        value = value + pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
 }
   return value;
@@ -44,7 +44,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double sum = 0;
-  for (int i = 1, n = 1; i <= count; ++i, ++n) {
+  for (uint16_t i = 1, n = 1; i <= count; ++i, ++n) {
     sum += pown(-1, n - 1) * calcItem(x, 2 * n - 2);
   }
   return sum;
